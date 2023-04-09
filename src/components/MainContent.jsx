f772ccaf4b16809ad9bf4e1c47daa5e0a712b55d
Prog.Hasan <abedalqaderhassan@gmail.com>
2023-04-09 10:31:45 +0300

    add condition

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,20 +4,18 @@ import { Button } from "react-bootstrap";
 import "../style/main.css";
 
 const MainContent = () => {
-  const [url, setUrl] = useState(
-    "https://images.dog.ceo/breeds/airedale/n02096051_441.jpg"
-  );
+  const [url, setUrl] = useState("");
 
   const getData = async () => {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
-    const {message} = await response.json();
+    const { message } = await response.json();
     setUrl(message);
   };
 
   return (
     <div className="mainContent">
-      <CustomeCard url={url} />
-      <Button as="input" type="button" value="Random" onClick={getData}  />
+      {url && <CustomeCard url={url} />}
+      <Button as="input" type="button" value="Random" onClick={getData} />
     </div>
   );
 };
